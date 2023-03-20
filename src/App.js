071e17489637d926f75509f5687284a53fcb650b
Tushar Saxena <tsaxena3k@gmail.com>
2023-03-20 15:48:27 +0530

    Musical player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Howl, Howler } from "howler";
+import newSound from "./assets/audio/sample.mp3";
+
+const playlist = {
+  happy: [newSound],
+  sad: [
+    "https://open.spotify.com/track/0cx3DUvLuL7iTJ6aUzAZNx?si=9c88991ff1da4bf7&nd=1",
+  ],
+  angry: [
+    "https://example.com/angry-song-1.mp3",
+    "https://example.com/angry-song-2.mp3",
+    "https://example.com/angry-song-3.mp3",
+  ],
+};
 
 function App() {
+  const [mood, setMood] = useState("happy");
+  const [songIndex, setSongIndex] = useState(0);
+
+  const sound = new Howl({
+    src: playlist[mood],
+    onend: () => {
+      setSongIndex((prevIndex) => prevIndex + 1);
+    },
+  });
+
+  useEffect(() => {
+    sound.load();
+    sound.play();
+
+    return () => {
+      sound.stop();
+    };
+  }, [songIndex]);
+
+  const handleMoodChange = (event) => {
+    setMood(event.target.value);
+    setSongIndex(0);
+  };
+
+  const handlePlay = () => {
+    sound.play();
+  };
+
+  const handlePause = () => {
+    sound.pause();
+  };
+
+  const handleNext = () => {
+    setSongIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handlePrev = () => {
+    setSongIndex((prevIndex) => prevIndex - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Music Mood Player</h1>
+      <form>
+        <label>
+          Select a mood:
+          <select value={mood} onChange={handleMoodChange}>
+            <option value="happy">Happy</option>
+            <option value="sad">Sad</option>
+            <option value="angry">Angry</option>
+          </select>
+        </label>
+      </form>
+      <div>
+        <button onClick={handlePrev}>Prev</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
     </div>
   );
 }
