10e1074ba7f692d8b05c09ca1ab852e55688b46b
Tushar Saxena <tsaxena3k@gmail.com>
2023-03-25 14:12:03 +0530

    nav fixes

--- src/Component/Team.js
+++ src/Component/Team.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import '../assets/styles/Team.scss'
-import rythm from '../assets/img/rythm.jpg'
-import tushar from '../assets/img/tushar.jpg'
-import meghna from '../assets/img/meghna.jpg'
-import rishab from '../assets/img/rishab.jpg'
+import React from "react";
+import "../assets/styles/Team.scss";
+import rythm from "../assets/images/rythm.jpg";
+import tushar from "../assets/images/tushar.jpg";
+import meghna from "../assets/images/meghna.jpg";
+import rishab from "../assets/images/rishab.jpg";
 
 const Team = () => {
   return (
-    <div className='teammain'>
-        <h1 className='heading'>Meet Our Team : </h1>
-        <div className="teamlist">
-            <div className="teamcard">
-                <img src={rythm} alt=""  />
-                Rythm | React Developer
-            </div>
-            <div className="teamcard">
-                  <img src={tushar} alt="" />
-                  Tushar | React Developer
-            </div>
-            <div className="teamcard">
-                  <img src={meghna} alt="" />
-                  Meghna | React Developer
-            </div>
-            <div className="teamcard">
-                  <img src={rishab} alt="" />
-                  Rishab | Data Scientist
-            </div>
+    <div className="teammain">
+      <h1 className="heading text-center mb-5">Meet Our Team</h1>
+      <div className="teamlist">
+        <div className="teamcard">
+          <img src={rythm} alt=""  className="mb-3"/>
+          Rythm | React Developer
         </div>
+        <div className="teamcard">
+          <img src={tushar} alt="" className="mb-3" />
+          Tushar | React Developer
+        </div>
+        <div className="teamcard">
+          <img src={meghna} alt="" className="mb-3" />
+          Meghna | React Developer
+        </div>
+        <div className="teamcard">
+          <img src={rishab} alt="" className="mb-3" />
+          Rishab | Data Scientist
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
